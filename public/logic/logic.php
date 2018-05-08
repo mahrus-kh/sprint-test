7c2ec47afdadbd6483968c33a7dfcac82b4f2616
mahrus-kh <mahrus.developer@gmail.com>
2018-05-08 14:21:48 +0700

    logic new

--- public/logic/logic.php
+++ public/logic/logic.php
@@ -3,5 +3,20 @@
  * Created by PhpStorm.
  * User: mahruskh
  * Date: 08/05/18
- * Time: 14:17
+ * Time: 9:20
  */
+
+$s = [2,1,6,9,4,3];
+
+$second = array_values(array_unique($s));
+rsort($second);
+var_dump($second[1]);
+
+$max = max($s);
+$second = null;
+foreach ($s as $value) {
+    if ($value < $max && $value > $second ){
+        $second = $value;
+    }
+}
+var_dump($second);
